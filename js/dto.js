318a1308a3fc7560369e39248152885c9ce66379
nowfilmustaq <nowfilmustaq@gmail.com>
2024-09-20 11:45:49 +0400

    Initial commit

---
+++ js/dto.js
@@ -0,0 +1,73 @@
+export const dto = (() => {
+    /**
+     * Generates a base response object containing code, data, and error properties.
+     * 
+     * @template T
+     * @param {number} code
+     * @param {T} data
+     * @param {string[]=} error
+     * @returns {{code: number, data: T, error?: string[]}} The response object containing the code, data, and error.
+     */
+    const baseResponse = (code, data, error) => {
+        return {
+            code,
+            data,
+            error,
+        };
+    };
+
+    const likeCommentResponse = ((love = 0) => {
+        return {
+            love,
+        };
+    });
+
+    const postCommentResponse = (({ uuid, own, name, presence, comment, created_at }) => {
+        let is_admin;
+        let comments = [];
+        let like = likeCommentResponse();
+
+        return {
+            uuid,
+            own,
+            name,
+            presence,
+            comment,
+            is_admin,
+            created_at,
+            comments,
+            like,
+        };
+    });
+
+    const statusResponse = (({ status }) => {
+        return {
+            status,
+        };
+    });
+
+    const commentShowMore = ((uuid, show = false) => {
+        return {
+            uuid,
+            show,
+        }
+    });
+
+    const postCommentRequest = ((id, name, presence, comment) => {
+        return {
+            id,
+            name,
+            presence,
+            comment,
+        };
+    });
+
+    return {
+        baseResponse,
+        statusResponse,
+        likeCommentResponse,
+        postCommentResponse,
+        commentShowMore,
+        postCommentRequest,
+    };
+})();
